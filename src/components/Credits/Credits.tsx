97c30a57f4ae8f4c38f14c39693c6ced69c0f7bc
Majuks <mariajnascimento04@gmail.com>
2025-04-23 22:40:15 -0300

    a

--- src/components/Credits/Credits.tsx
+++ src/components/Credits/Credits.tsx
@@ -5,7 +5,7 @@ function Credits() {
 
     return(
         <>
-            <div className="inline-flex flex-col items-center justify-center mt-10 [background-color:#3b2a1a] [background-image:radial-gradient(rgba(255,255,255,0.04)_1px,transparent_1px)] [background-size:6px_6px] [background-repeat:repeat]  border-4 border-yellow-700 rounded-md p-4 px-10 shadow-md w-[440px] ">
+            <div className="inline-flex  flex-col items-center justify-center mt-10 [background-color:#3b2a1a] [background-image:radial-gradient(rgba(255,255,255,0.04)_1px,transparent_1px)] [background-size:6px_6px] [background-repeat:repeat]  border-4 border-yellow-700 rounded-md p-4 px-10 shadow-md w-[440px] ">
                 <h1 className="text-[2.2em] leading-[1.1] pb-10 pt-10  text-yellow-100 " style={{ fontFamily:  "'Garamond', serif"}}>CRÉDITOS</h1>
                 <div className="p-10">
                     <h3 className="text-[1.9em] leading-[1.1] mb-2 text-[#BACEE2]" style={{ fontFamily:  "'Garamond', serif"}}>Desenvolvimento</h3>
